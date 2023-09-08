065820c865fed882b9ffcbce8825eb7c8d799746
Pratham Sitoula <psitoula2003@gmail.com>
2023-09-07 19:21:00 -0600

    mongo setup

--- server.js
+++ server.js
@@ -1,1 +1,36 @@
-console.log('test');
+import * as dotenv from 'dotenv';
+dotenv.config();
+import express from 'express';
+import mongoose from 'mongoose';
+import morgan from 'morgan';
+const app = express();
+
+app.use(express.json());
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+const port = process.env.PORT || 3000;
+
+
+app.use((err, req, res, next) => {
+  res.status(500).json({ error: err.message });
+});
+
+app.use("*", (req, res) => res.status(404).json({ error: "not found" }));
+
+
+try {
+  mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
+  app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
+} catch (error) {
+  console.log(error);
+}
+
+
+
